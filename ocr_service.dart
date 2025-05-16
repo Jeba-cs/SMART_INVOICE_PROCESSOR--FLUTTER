50b44d9b0eb0f202e43665713a6f7312a9182d5c
Jeba-cs <jeba.b@cstechnologiescorp.com>
2025-05-16 10:44:06 +0530

    Update ocr_service.dart

--- ocr_service.dart
+++ ocr_service.dart
@@ -1,12 +1,21 @@
 import 'dart:io';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import 'package:syncfusion_flutter_pdf/pdf.dart';
 
 class OCRService {
   Future<String> extractText(File imageFile) async {
     final inputImage = InputImage.fromFile(imageFile);
-    final textRecognizer = TextRecognizer();
-    final RecognizedText recognizedText = await textRecognizer.processImage(inputImage);
+    final textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
+    final recognizedText = await textRecognizer.processImage(inputImage);
     await textRecognizer.close();
     return recognizedText.text;
+  }
+
+  Future<String> extractTextFromPdf(File pdfFile) async {
+    final bytes = await pdfFile.readAsBytes();
+    final document = PdfDocument(inputBytes: bytes);
+    String text = PdfTextExtractor(document).extractText();
+    document.dispose();
+    return text;
   }
 }
